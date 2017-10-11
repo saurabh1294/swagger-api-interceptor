504ff2a525b0026060c7589d67a7db9d727b081b
sgattani <saurabh.gattani@gmail.com>
2017-10-11 16:20:14 +0530

    initial commit of swagger API interceptor

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # swagger-api-interceptor
 This intercepts API calls based on swagger file and returns full response of the endpoint as well as response based on HTTP code
+
+
+Install nodejs from http://nodejs.org
+
+Then from your project directory, fire the below commands
+
+1) npm install express
+
+2) npm install body-parser
+
+3) npm install swagger-parser
+
+4) npm install yamljs
+
+5) npm install js-beautify
+
+6) npm install circular-json
+
+7) npm install swagger-express-middleware
---
+++ app.js
@@ -0,0 +1,190 @@
+/*
+	*File: app.js
+	*Author: Saurabh Gattani
+	*Last Modified: 25th September 2017
+	*Revised on: 25th September 2017
+*/
+
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var SwaggerParser = require('swagger-parser');
+var YAML = require('yamljs');
+var CircularJSON = require('circular-json');
+var jsBeautify = require('js-beautify');
+var swagger = require('swagger-express-middleware');
+var path       = require('path');
+var port = 8000;
+
+var Middleware      = swagger.Middleware;
+var middleware = new Middleware(app);
+
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+/*app.post('/intercept', function(req, res) 
+{
+	if (req.body.type === 'YAMLText') {
+		nativeObject = YAML.parse(req.body.yamlString);
+		//res.send({output:nativeObject});
+		SwaggerParser.bundle(nativeObject).then(function(api) {
+			//console.log("Intercepting Swagger to JSON bundle", api);
+			res.send({output:api});
+		});
+	} else {
+		SwaggerParser.bundle(req.body.filename).then(function(api) {
+			//console.log("Intercepting Swagger to JSON bundle", api);
+			res.send({output:api});
+		});
+	}
+});
+
+
+
+
+app.post('/parse', function(req, res) 
+{
+	if (req.body.type === 'YAMLText') {
+		nativeObject = YAML.parse(req.body.yamlString);
+		//res.send({output:nativeObject});
+		SwaggerParser.parse(nativeObject).then(function(api) {
+			//console.log("Intercepting Swagger to JSON bundle", api);
+			res.send({output:api});
+		});
+	} else {
+		SwaggerParser.parse(req.body.filename).then(function(data) {
+			//console.log("Parsed swagger : ", data);
+			res.send({output:data});
+		});
+	}
+});
+
+app.post('/dereference', function(req, res) 
+{
+	if (req.body.type === 'YAMLText') {
+		nativeObject = YAML.parse(req.body.yamlString);
+		//res.send({output:nativeObject});
+		SwaggerParser.dereference(nativeObject).then(function(api) {
+			//console.log("Intercepting Swagger to JSON bundle", api);
+			res.send({output:api});
+		});
+	} else {
+		SwaggerParser.dereference(req.body.filename).then(function(data) {
+		//console.log("Dereferenced swagger : ", data);
+		res.send({output:data});
+		});
+	}
+});
+
+app.post('/resolve', function(req, res) 
+{
+	if (req.body.type === 'YAMLText') {
+		nativeObject = YAML.parse(req.body.yamlString);
+		//res.send({output:nativeObject});
+		SwaggerParser.resolve(nativeObject).then(function(data) {
+			var serialized = CircularJSON.stringify(data);
+			//console.log("Resolved swagger : ", data, serialized);
+			res.send(jsBeautify.js_beautify(serialized));
+		});
+	} else {
+		SwaggerParser.resolve(req.body.filename).then(function(data) {
+			var serialized = CircularJSON.stringify(data);
+			//console.log("Resolved swagger : ", data, serialized);
+			res.send(jsBeautify.js_beautify(serialized));
+		});
+	}
+});
+
+app.post('/YAMLToJSONObject', function(req, res) 
+{
+	// parse YAML string
+	//console.log("YAML = ", req.body.yamlString, typeof req.body.yamlString);
+	nativeObject = YAML.parse(req.body.yamlString);
+	res.send({output:nativeObject});
+});
+
+app.get('/', function(req, res) 
+{
+    res.sendfile("./index.html");
+});
+
+console.log("Listening at "+port)
+app.listen(port);*/
+
+
+app.post('/YAMLToJSONObject', function(req, res) 
+{
+	// parse YAML string
+	//console.log("YAML = ", req.body.yamlString, typeof req.body.yamlString);
+	nativeObject = YAML.parse(req.body.yamlString);
+	res.send({output:nativeObject});
+});
+
+
+middleware.init(path.join(__dirname, 'product-offerings-PPV_YAML.yaml'), function(err) {
+  // Enable Express' case-sensitive and strict options
+  // (so "/pets/Fido", "/pets/fido", and "/pets/fido/" are all different)
+  app.enable('case sensitive routing');
+  app.enable('strict routing');
+
+  app.use(middleware.metadata());
+  app.use(middleware.files(
+    {
+      // Override the Express App's case-sensitive and strict-routing settings
+      // for the Files middleware.
+      caseSensitive: false,
+      strict: false
+    },
+    {
+      // Serve the Swagger API from "/swagger/api" instead of "/api-docs"
+      apiPath: '/swagger/api',
+
+      // Disable serving the "PetStore.yaml" file
+      rawFilesPath: false
+    }
+  ));
+
+  app.use(middleware.parseRequest(
+    {
+      // Configure the cookie parser to use secure cookies
+      cookie: {
+        secret: 'MySuperSecureSecretKey'
+      },
+
+      // Don't allow JSON content over 100kb (default is 1mb)
+      json: {
+        limit: '100kb'
+      }
+    }
+  ));
+
+  // These two middleware don't have any options (yet)
+  app.use(
+    middleware.CORS(),
+    middleware.validateRequest()
+  );
+
+  // Add custom middleware
+  app.post('/intercept', function(req, res, next) {
+    console.log("API call to "+req.body.endpoint+" intercepted here");
+  });
+
+  
+
+  // Add a custom error handler that returns errors as HTML
+  app.use(function(err, req, res, next) {
+    res.status(err.status);
+    res.type('html');
+    res.send(util.format('<html><body><h1>%d Error!</h1><p>%s</p></body></html>', err.status, err.message));
+  });
+
+  app.listen(8000, function() {
+    console.log('The Swagger Interceptor is now running at http://localhost:8000');
+  });
+});
+
+
